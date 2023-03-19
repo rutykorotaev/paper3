05c74200c078de8230c0e453d83ac249c04ecb1f
rutykorotaev@mail.utoronto.ca <ruty.korotaiev@mail.utoronto.ca>
2023-03-19 09:15:36 -0400

    Changes

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -6,16 +6,43 @@
 
 #### Workspace setup ####
 library(opendatatoronto)
-library(tidyverse)
+library(haven)
 
 #### Download data ####
 
+# Download & unzip all the GSS data across the years
+zip_file <- "inputs/data/large_files/GSS_stata.zip"
 
-#### Save data ####
+download.file("https://gss.norc.org/documents/stata/GSS_stata.zip", zip_file)
+unzip(zip_file, exdir = "inputs/data/large_files")
+
+# read dta data and write to csv
+raw_gss_data <- read_dta("inputs/data/large_files/gss7221_r3a.dta")
 
 
+## Filter and save data related to respondent info and survey questions
 
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# Respondent information
+raw_respondent_info <-
+  raw_gss_data |>
+  select(
+    year,
+    id,
+    jewaj,
+    othjew,
+    jew,
+    relitena,
+    age,
+    spjew,
+    denom,
+    jew16,
+    spjew16,
+    spothjew
+  )
+
+write_csv(
+  x = raw_jew_info,
+  file = "inputs/data/raw_jew_info.csv"
+)
 
          
