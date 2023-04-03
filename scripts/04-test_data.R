0fdb755c02878b2157fb82de12fa05969681cefb
rutykorotaev@mail.utoronto.ca <ruty.korotaiev@mail.utoronto.ca>
2023-04-03 09:31:10 -0400

    New changes

--- scripts/04-test_data.R
+++ scripts/04-test_data.R
@@ -7,12 +7,21 @@
 # Workspace setup
 library(tidyverse)
 
-#Test 1
+#Test 1: Use unique function to ensure there are no duplicates in dataset
+clean_respondent_data |> unique()
 
-#Test 2
+#Test 2: Use validator and is.numeric function to ensure all values are regarded as numeric
 
-#Test 3
-
+library(validate)
+rules <- validator(
+  is.numeric(relig),
+  is.numeric(year),
+  is.numeric(jew),
+  is.numeric(spjew),
+  is.numeric(jew16),
+  is.numeric(spjew16),
+  is.numeric(marital)
+)
 
 #Lint script
 library(lintr)
